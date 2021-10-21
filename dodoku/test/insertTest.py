8f6ac17aa5ec2082b46f9ff5f63ea822ab3c66ce
Mingcong Li <jacks@Lee-Alienware>
2021-10-21 17:39:44 -0500

    Revert "Red Light | "
    This reverts commit 1d7d0e1fe50ef03ecf97e0038883df70e01907f0.

--- dodoku/test/insertTest.py
+++ dodoku/test/insertTest.py
@@ -7,6 +7,15 @@ import http.client
 import dodoku.insert as insert
 from dodoku.create import _get_integrity, _getcolumn, _get_grid_sha256
 
-    def test_valid_insert_1(self):
-        self._test_valid_insert(1,  1,  6)
-
+class InsertTest(TestCase):
+    def setUp(self) -> None:
+        self.PATH = "/dodoku?"
+        self.PORT = 5000
+        self.URL = "localhost"
+        self.gridKey = 'grid'
+        self.integrityKey = 'integrity'
+        self.statusKey = 'status'
+        self.statusOk = 'ok' 
+        self.statusWarning = 'warning'
+        self.statusError = 'error:' 
+         

f2191bc7a519e3636f16181260570c9367f78afc
Mingcong Li <jacks@Lee-Alienware>
2021-12-07 09:40:07 -0600

    Red Light |

--- dodoku/recommend.py
+++ dodoku/recommend.py
@@ -1,3 +1,72 @@
+import json
+import re
 def _recommend(parms):
-    result = {'status': 'recommend stub'}
+    raw_text_grid = parms["grid"]
+    raw_text_cell = parms["cell"]
+    ok, grid = _parse_grid(raw_text_grid)
+    board = _gridlist_to_board(grid)
+
+    ok, cell = _parse_cell(raw_text_cell)
+    r, c = cell
+    recommend_numbers = _get_recommendation_numbers(board,r,c)
+    result = {'recommendation': recommend_numbers,'status':'ok'}
     return result
+
+def _parse_grid(raw_grid_text):
+    try:
+        grid = json.loads(raw_grid_text)
+        return True, grid
+    except:
+        return False, None
+
+def _parse_cell(raw_cell_text):
+    pat = r"^[rR](\d\d?)[cC](\d\d?)$"
+    m = re.fullmatch(pat, raw_cell_text)
+    if m is None:
+        return False, None
+    r = int(m[1])
+    c = int(m[2])
+    return True, (r, c)
+
+def _gridlist_to_board(grid):
+    board = []
+    cur_index = 0
+    for _ in range(6):
+        row = grid[cur_index : cur_index + 9]
+        row = row + [None] * 6
+        board.append(row)
+
+        cur_index += 9
+    for _ in range(3):
+        board.append(grid[cur_index : cur_index + 15])
+        cur_index += 15
+    for _ in range(6):
+        row = grid[cur_index : cur_index + 9]
+        row = [None] * 6 + row
+        board.append(row)
+        cur_index += 9
+    return board
+
+def _get_recommendation_numbers(board,r,c):
+    if board[r-1][c-1]<0:
+        return []
+    board[r-1][c-1] = 0
+    all_numbers = set(range(1,10))
+    row_numbers = set()
+    for num in board[r-1]:
+        if num:
+            row_numbers.add(abs(num))
+    col_numbers = set()
+    for row in range(0,15):
+        if board[row][c-1]:
+            col_numbers.add(abs(board[row][c-1]))
+    subgrid_numbers = set()
+    row_start_index = (r-1)//3*3
+    col_start_index = (c-1)//3*3
+    for row in range(row_start_index, row_start_index+3):
+        for col in range(col_start_index, col_start_index+3):
+            if board[row][col]:
+                subgrid_numbers.add(abs(board[row][col]))
+    numbers = row_numbers.union(col_numbers).union(subgrid_numbers)
+    recommend_numbers = (all_numbers - numbers)
+    return sorted(list(recommend_numbers))
--- dodoku/test/recommendTest.py
+++ dodoku/test/recommendTest.py
@@ -1,5 +1,45 @@
 from unittest import TestCase
 import dodoku.status as recommend 
+import dodoku.recommend as recommend 
 
 class RecommendTest(TestCase):
         pass
+    #Happy path test
+    def test100_010NormalInput(self):
+        expectedResult = {'recommendation': [3,8],'status':'ok'}      
+        parms = {'op': 'recommend', 'cell': 'r7c9', 
+                 'grid': '[0,-2,0,0,-1,0,0,-4,0,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]', 
+                 'integrity': '5a3f0c31'}
+        result = recommend._recommend(parms)
+        self.assertEqual(expectedResult['recommendation'], result['recommendation'])
+        self.assertEqual(expectedResult['status'], result['status'])
+
+    #Happy path test
+    def test100_020NormalInput(self):
+        expectedResult = {'recommendation': [],'status':'ok'}      
+        parms = {'op': 'recommend', 'cell': 'r1c1', 
+                 'grid': '[0,-2,5,7,-1,6,9,-4,3,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]', 
+                 'integrity': '8b3504ae'}
+        result = recommend._recommend(parms)
+        self.assertEqual(expectedResult['recommendation'], result['recommendation'])
+        self.assertEqual(expectedResult['status'], result['status'])     
+
+    #Happy path test
+    def test100_030NormalInput(self):
+        expectedResult = {'recommendation': [],'status':'ok'}      
+        parms = {'op': 'recommend', 'cell': 'r1c2', 
+                 'grid': '[0,-2,5,7,-1,6,9,-4,3,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]', 
+                 'integrity': '8b3504ae'}
+        result = recommend._recommend(parms)
+        self.assertEqual(expectedResult['recommendation'], result['recommendation'])
+        self.assertEqual(expectedResult['status'], result['status']) 
+
+    #Happy path test
+    def test100_040NormalInput(self):
+        expectedResult = {'recommendation': [5],'status':'ok'}      
+        parms = {'op': 'recommend', 'cell': 'r1c3', 
+                 'grid': '[0,-2,5,7,-1,6,9,-4,3,-8,0,-1,-9,0,0,0,0,-5,0,0,0,0,-3,0,0,-1,0,0,-3,0,0,0,0,-4,0,-6,-5,0,-9,0,0,0,0,0,-7,0,0,0,0,0,0,-2,-8,0,-2,0,0,-6,0,0,0,0,0,0,-1,-4,0,-6,0,0,0,-6,0,0,-3,0,0,0,-2,0,0,-1,0,-9,0,-4,0,-5,-7,0,0,0,0,0,0,-7,0,0,-5,0,0,-6,0,0,0,0,-9,0,-2,0,0,0,0,0,-4,0,-8,-7,0,-9,0,0,0,0,0,0,0,-5,0,0,-9,0,0,0,0,-4,0,0,-6,0,-3,-9,0,0,0,-6,0,0,-5,0,0,-3,-1]', 
+                 'integrity': '8b3504ae'}
+        result = recommend._recommend(parms)
+        self.assertEqual(expectedResult['recommendation'], result['recommendation'])
+        self.assertEqual(expectedResult['status'], result['status'])
